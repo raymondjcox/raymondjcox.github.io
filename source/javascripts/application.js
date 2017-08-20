f1b214dedda4fab12174669d33e68dfaf4c0fdfb
Raymond Cox <raymond@helios.raymondjcox.com>
2017-08-19 22:28:35 -0400

    Use flexbox instead of bootstrap

--- source/javascripts/application.js
+++
@@ -1,9 +0,0 @@
-var mobileNav = document.querySelector('.mobile-nav');
-var mobileHamburger = document.querySelector('.mobile-hamburger');
-mobileHamburger.onclick = function() {
-  if (mobileNav.className.indexOf('hide') >= 0) {
-    mobileNav.className = mobileNav.className.replace('hide', 'show');
-  } else {
-    mobileNav.className = mobileNav.className.replace('show', 'hide');
-  }
-};
